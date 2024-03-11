1f9b65142bbf3d7314ef2f19b368a1ae423246ac
VEGAMETA <viperigor12@gmail.com>
2024-03-11 11:32:24 +0300

    fixed Exception

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ VIDEO_CAPTURE_SOURCE = 0
 async def main():
     cap = cv2.VideoCapture(VIDEO_CAPTURE_SOURCE)
     hue = cap.get(cv2.CAP_PROP_HUE)
-    print("Start hue is ", hue)
+    print("Started")
     while True:
-        try:
-            hue += 1
-            if hue == 180:
-                hue = -hue
-
-            cap.set(cv2.CAP_PROP_HUE, hue)
-            await asyncio.sleep(0.01)
-        except KeyboardInterrupt:
-            quit("exited")
+        hue += 1
+        if hue >= 180:
+            hue = -hue
+        cap.set(cv2.CAP_PROP_HUE, hue)
+        await asyncio.sleep(0.01)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        quit("Exited")
